0373e12f1a0703be1a2b81286b2b8e5a9c174056
SwissQuant <rico@nemo.ventures>
2024-04-05 19:49:02 +0100

    fix formatting

--- louvre/coingecko/__init__.py
+++ louvre/coingecko/__init__.py
@@ -1,1 +1,2 @@
 from .client import Coingecko
+from .format import convert_historical_data_to_dataframe
--- louvre/coingecko/format.py
+++ louvre/coingecko/format.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 def convert_historical_data_to_dataframe(data):
     # Convert to DataFrame
-    df = pd.DataFrame(data, columns=['Timestamp', 'Value'])
+    df = pd.DataFrame(data, columns=['time', 'value'])
     
     # Convert the 'Timestamp' column from milliseconds since the Unix epoch to datetime
-    df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='ms')
+    df['time'] = pd.to_datetime(df['time'], unit='ms')
+    df = df.set_index("time")
     
-    return df
+    return df["value"]
